77eaed6c2e1a86456555ec59bc15ab35d1b4c9f1
Dorian Gosse <Dorian.richard@hotmail.com>
2022-09-04 16:21:58 -0500

    Impl button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,15 @@
-import React from 'react' 
+import React from 'react'
+import { Button as BootstrapButton } from 'react-bootstrap'
 
-export default Button = ({ label, ...props}) => (
-  <button {...props}>
-    {label}
-  </button>
+import './Button.css'
+
+const classNameByType = {
+  search: 'search',
+  moiveBtn: 'movie-result-btn'
+}
+
+export const Button = ({ label, type, ...props}) => (
+  <BootstrapButton className={classNameByType[type]} {...props}>
+    { label }
+  </BootstrapButton>
 )
