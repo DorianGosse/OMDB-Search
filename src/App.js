b80c94daec3488e8fdd5c7756c983f8f4ba2f257
Dorian Gosse <Dorian.richard@hotmail.com>
2022-09-04 16:39:40 -0500

    WIP - initial impl Movie Search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import MovieSearch from './components/Search/MovieSearch'
 
 function App() {
   return (
     <div className="App">
-      
+      <MovieSearch />
     </div>
   )
 }
---
+++ src/components/Search/MovieSearch.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { Button } from '../Button'
+import { Input } from '../Input'
+
+import './MovieSearch.css'
+
+const MovieSearch = ({}) => {
+  return (
+    <Container>
+      <Button label='something' />
+      <Input />
+    </Container>
+  )
+}
+
+export default MovieSearch
